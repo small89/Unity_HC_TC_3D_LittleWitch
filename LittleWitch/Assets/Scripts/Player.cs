fbf5dd41555c00b015be8db7a6dd7acfa5e641b1
KID <50666207+KID421@users.noreply.github.com>
2021-01-28 21:28:56 +0800

    攝影機

--- LittleWitch/Assets/Scripts/Player.cs
+++ LittleWitch/Assets/Scripts/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
     public float jump = 10;
     [Header("旋轉速度"), Range(0, 1000)]
     public float turn = 10;
+    [Header("攝影機角度限制")]
+    public Vector2 camLimit = new Vector2(-30, 0);
 
     /// <summary>
     /// 是否在地面上
@@ -16,6 +18,8 @@ public class Player : MonoBehaviour
     private Animator ani;
     private Rigidbody rig;
     private Transform cam;
+    private float x;
+    private float y;
 
     //喚醒事件：在 Start 前執行一次
     private void Awake()
@@ -36,13 +40,15 @@ public class Player : MonoBehaviour
     /// </summary>
     private void Move()
     {
-        float v = Input.GetAxis("Vertical");            // 取得 前後軸 值 W S 上 下
-        float h = Input.GetAxis("Horizontal");          // 取得 前後軸 值 A D 左 右
+        float v = Input.GetAxis("Vertical");                                // 取得 前後軸 值 W S 上 下
+        float h = Input.GetAxis("Horizontal");                              // 取得 前後軸 值 A D 左 右
 
-        // 加速度 = 前方 * 前後值 + 右方 * 左右值 * 速度 * 1/60 + 上方 * 加速度上下值
-        rig.velocity = ((transform.forward * v + transform.right * h) * speed * Time.deltaTime) + transform.up * rig.velocity.y;
-        // 動畫.設定布林值("參數名稱"，剛體.加速度.值 > 0)
-        ani.SetBool("跑步開關", rig.velocity.magnitude > 0);
+        Transform camNew = cam;                                             // 新攝影機座標資訊
+        camNew.eulerAngles = new Vector3(0, cam.eulerAngles.y, 0);          // 去掉 X 與 Z 角度
+
+        rig.velocity = ((camNew.forward * v + transform.right * h) * speed * Time.deltaTime) + transform.up * rig.velocity.y;       // 加速度 = 前方 * 前後值 + 右方 * 左右值 * 速度 * 1/60 + 上方 * 加速度上下值
+
+        ani.SetBool("跑步開關", rig.velocity.magnitude > 0);                // 動畫.設定布林值("參數名稱"，剛體.加速度.值 > 0)
     }
 
     /// <summary>
@@ -50,6 +56,9 @@ public class Player : MonoBehaviour
     /// </summary>
     private void TurnCamera()
     {
-
+        x += Input.GetAxis("Mouse X") * turn * Time.deltaTime;  // 取得滑鼠 X 值
+        y -= Input.GetAxis("Mouse Y") * turn * Time.deltaTime;  // 取得滑鼠 Y 值
+        y = Mathf.Clamp(y, camLimit.x, camLimit.y);             // 限制 Y
+        cam.localEulerAngles = new Vector3(y , x, 0);           // 攝影機.角度 = (Y 值，X 值，0) * 旋轉速度 * 1/60
     }
 }
