55a03142b14b66a7ada86dd49753f8c562da7876
KID <50666207+KID421@users.noreply.github.com>
2021-01-28 20:21:10 +0800

    角色移動控制

---
+++ LittleWitch/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [Header("追蹤速度"), Range(0, 300)]
+    public float speed = 10;
+
+    /// <summary>
+    /// 目標
+    /// </summary>
+    private Transform target;
+
+    private void Awake()
+    {
+        target = GameObject.Find("玩家").transform;
+    }
+
+    // 延遲更新：在 Update 每一幀之後執行
+    private void LateUpdate()
+    {
+        Track();
+    }
+
+    /// <summary>
+    /// 追蹤
+    /// </summary>
+    private void Track()
+    {
+        Vector3 posTarget = target.position;    // 目標的座標
+        Vector3 posCamera = transform.position; // 攝影機的座標
+
+        // 攝影機的新座標 = 三維向量.往前移動(攝影機的座標，目標的座標，速度 * 1/60)
+        posCamera = Vector3.MoveTowards(posCamera, posTarget, speed * Time.deltaTime);
+        // 攝影機的座標 = 攝影機的新座標
+        transform.position = posCamera;
+    }
+}
--- LittleWitch/Assets/Scripts/Player.cs
+++ LittleWitch/Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
     public float speed = 10;
     [Header("跳躍高度"), Range(0, 1000)]
     public float jump = 10;
+    [Header("旋轉速度"), Range(0, 1000)]
+    public float turn = 10;
 
     /// <summary>
     /// 是否在地面上
@@ -13,23 +15,40 @@ public class Player : MonoBehaviour
     private bool isGround;
     private Animator ani;
     private Rigidbody rig;
+    private Transform cam;
 
     //喚醒事件：在 Start 前執行一次
     private void Awake()
     {
         ani = GetComponent<Animator>();
         rig = GetComponent<Rigidbody>();
+        cam = GameObject.Find("攝影機根物件").transform;
     }
 
     private void Update()
     {
         Move();
+        TurnCamera();
     }
 
     /// <summary>
     /// 移動方法
     /// </summary>
     private void Move()
+    {
+        float v = Input.GetAxis("Vertical");            // 取得 前後軸 值 W S 上 下
+        float h = Input.GetAxis("Horizontal");          // 取得 前後軸 值 A D 左 右
+
+        // 加速度 = 前方 * 前後值 + 右方 * 左右值 * 速度 * 1/60 + 上方 * 加速度上下值
+        rig.velocity = ((transform.forward * v + transform.right * h) * speed * Time.deltaTime) + transform.up * rig.velocity.y;
+        // 動畫.設定布林值("參數名稱"，剛體.加速度.值 > 0)
+        ani.SetBool("跑步開關", rig.velocity.magnitude > 0);
+    }
+
+    /// <summary>
+    /// 旋轉攝影機
+    /// </summary>
+    private void TurnCamera()
     {
 
     }
